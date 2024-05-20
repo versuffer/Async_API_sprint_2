a2141a9e88964137865c6c7340bbb2a6f46d209c
Яна Бубнова <bujhvh@mail.ru>
2024-05-20 22:20:22 +0300

    test_film_get

--- tests/functional/src/test_films.py
+++ tests/functional/src/test_films.py
@@ -1,14 +1,18 @@
+import uuid
+
 import pytest
+from fastapi import status
 from httpx import AsyncClient
 
+from tests.functional.testdata.genres_data import es_genres_index_mapping, genres_data
 from tests.functional.testdata.movie_data import es_movie_index_mapping, movie_data
 
 pytestmark = pytest.mark.anyio
 
 
 class TestFilms:
-
-    INDEX: str = 'movies'
+    MOVIE_INDEX: str = 'movies'
+    GENRE_INDEX: str = 'genres'
 
     @pytest.mark.parametrize(
         'query_data, expected_answer',
@@ -22,12 +26,41 @@ class TestFilms:
     ):
 
         es_write_data(
-            es_index=self.INDEX,
+            es_index=self.MOVIE_INDEX,
             index_mapping=es_movie_index_mapping,
-            data=es_prepared_data(index=self.INDEX, data=movie_data),
+            data=es_prepared_data(index=self.MOVIE_INDEX, data=movie_data),
         )
 
         response = await async_test_client.get('/api/v1/films/search', params=query_data)
 
         assert response.status_code == expected_answer['status']
         assert len(response.json()) == expected_answer['length']
+
+    @pytest.mark.parametrize(
+        "movie_id, expected_status_code",
+        [
+            (movie_data[0].get("id"), status.HTTP_200_OK),
+            (str(uuid.uuid4()), status.HTTP_404_NOT_FOUND),
+            ("unknown", status.HTTP_422_UNPROCESSABLE_ENTITY),
+            (-8, status.HTTP_422_UNPROCESSABLE_ENTITY),
+        ],
+
+    )
+    async def test_film_get(
+            self, async_test_client: AsyncClient, es_write_data, es_prepared_data, movie_id, expected_status_code
+    ):
+        es_write_data(
+            es_index=self.MOVIE_INDEX,
+            index_mapping=es_movie_index_mapping,
+            data=es_prepared_data(index=self.MOVIE_INDEX, data=movie_data),
+        )
+
+        es_write_data(
+            es_index=self.GENRE_INDEX,
+            index_mapping=es_genres_index_mapping,
+            data=es_prepared_data(index=self.GENRE_INDEX, data=genres_data),
+        )
+
+        response = await async_test_client.get(f"/api/v1/films/{movie_id}")
+
+        assert response.status_code == expected_status_code
