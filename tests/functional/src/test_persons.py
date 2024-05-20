4ae07aca7d66aaee032aea9f5f43fd70a599256d
Dmitrii Annsjaw <rotanin@gmail.com>
2024-05-20 12:28:50 +0530

    persons test

--- tests/functional/src/test_persons.py
+++ tests/functional/src/test_persons.py
@@ -1,19 +1,23 @@
+from uuid import UUID
+
 import pytest
 from httpx import AsyncClient
 
-from tests.functional.testdata.movie_data import es_persons_index, person_data
-
+from tests.functional.testdata.movie_data import es_movie_index_mapping, movie_data
+from tests.functional.testdata.person_data import es_persons_index, persons_from_film
+from app.main import app as fastapi_app
 pytestmark = pytest.mark.anyio
 
 
 class TestPersons:
 
-    INDEX: str = 'persons'
+    MOVIE_INDEX: str = 'movies'
+    PERSON_INDEX: str = 'persons'
 
     @pytest.mark.parametrize(
         'query_data, expected_answer',
         [
-            ({'search_query': 'Team', 'page': 1, 'page_size': 20}, {'status': 200, 'length': 10}),
+            ({'search_query': 'Ann', 'page': 1, 'page_size': 20}, {'status': 200, 'length': 1}),
             ({'search_query': 'Mashed potato'}, {'status': 200, 'length': 0}),
         ],
     )
@@ -22,12 +26,32 @@ class TestPersons:
     ):
 
         es_write_data(
-            es_index=self.INDEX,
+            es_index=self.PERSON_INDEX,
             index_mapping=es_persons_index,
-            data=es_prepared_data(index=self.INDEX, data=person_data),
+            data=es_prepared_data(index=self.PERSON_INDEX, data=persons_from_film),
         )
 
         response = await async_test_client.get('/api/v1/persons/search', params=query_data)
 
         assert response.status_code == expected_answer['status']
         assert len(response.json()) == expected_answer['length']
+
+    async def test_person_detail(self, async_test_client: AsyncClient, es_write_data, es_prepared_data):
+
+        es_write_data(
+            es_index=self.PERSON_INDEX,
+            index_mapping=es_persons_index,
+            data=es_prepared_data(index=self.PERSON_INDEX, data=persons_from_film),
+        )
+
+        es_write_data(
+            es_index=self.MOVIE_INDEX,
+            index_mapping=es_movie_index_mapping,
+            data=es_prepared_data(index=self.MOVIE_INDEX, data=movie_data),
+        )
+
+        person_id = persons_from_film[0].get('id')
+        response = await async_test_client.get(fastapi_app.url_path_for('get_person', person_id=person_id))
+
+        assert response.status_code == 200
+        assert len(response.json()) == 1
---
+++ tests/functional/testdata/person_data.py
@@ -0,0 +1,45 @@
+import uuid
+
+
+es_persons_index = {
+    "settings": {
+        "refresh_interval": "1s",
+        "analysis": {
+            "filter": {
+                "english_stop": {"type": "stop", "stopwords": "_english_"},
+                "english_stemmer": {"type": "stemmer", "language": "english"},
+                "english_possessive_stemmer": {"type": "stemmer", "language": "possessive_english"},
+                "russian_stop": {"type": "stop", "stopwords": "_russian_"},
+                "russian_stemmer": {"type": "stemmer", "language": "russian"},
+            },
+            "analyzer": {
+                "ru_en": {
+                    "tokenizer": "standard",
+                    "filter": [
+                        "lowercase",
+                        "english_stop",
+                        "english_stemmer",
+                        "english_possessive_stemmer",
+                        "russian_stop",
+                        "russian_stemmer",
+                    ],
+                }
+            },
+        },
+    },
+    "mappings": {
+        "dynamic": "strict",
+        "properties": {
+            "id": {"type": "keyword"},
+            "full_name": {"type": "text", "analyzer": "ru_en", "fields": {"raw": {"type": "keyword"}}},
+        },
+    },
+}
+
+persons_from_film = [
+    {'id': 'ef86b8ff-3c82-4d31-ad8e-72b69f4e3f95', 'full_name': 'Ann'},
+    {'id': 'fb111f22-121e-44a7-b78f-b19191810fbf', 'full_name': 'Bob'},
+    {'id': 'caf76c67-c0fe-477e-8766-3ab3ff2574b5', 'full_name': 'Ben'},
+    {'id': 'b45bd7bc-2e16-46d5-b125-983d356768c6', 'full_name': 'Howard'},
+    {'id': '5d33e65b-948c-4d73-a037-2779a105dd75', 'full_name': 'Stan'}
+]
