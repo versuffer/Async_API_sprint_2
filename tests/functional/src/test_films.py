8ace6ef9e5d2319da1c7776a741ca78bcd8d3e75
Yana Bubnova <102591631+Kasaress@users.noreply.github.com>
2024-05-21 22:31:05 +0300

    Issue 5 functional test film (#6)
    * test_film_get
    
    * test_films

--- tests/functional/src/test_films.py
+++ tests/functional/src/test_films.py
@@ -1,14 +1,18 @@
+import uuid
+
 import pytest
+from fastapi import status
 from httpx import AsyncClient
 
+from tests.functional.testdata.genres_data import es_genres_index_mapping, genres_data
 from tests.functional.testdata.movie_data import es_movie_index_mapping, movie_data
 
 pytestmark = pytest.mark.anyio
 
 
 class TestFilms:
-
-    INDEX: str = 'movies'
+    MOVIE_INDEX: str = 'movies'
+    GENRE_INDEX: str = 'genres'
 
     @pytest.mark.parametrize(
         'query_data, expected_answer',
@@ -22,12 +26,128 @@ class TestFilms:
     ):
 
         es_write_data(
-            es_index=self.INDEX,
+            es_index=self.MOVIE_INDEX,
             index_mapping=es_movie_index_mapping,
-            data=es_prepared_data(index=self.INDEX, data=movie_data),
+            data=es_prepared_data(index=self.MOVIE_INDEX, data=movie_data),
         )
 
         response = await async_test_client.get('/api/v1/films/search', params=query_data)
 
         assert response.status_code == expected_answer['status']
         assert len(response.json()) == expected_answer['length']
+
+    @pytest.mark.parametrize(
+        "movie_id, expected_status_code",
+        [
+            (str(uuid.uuid4()), status.HTTP_404_NOT_FOUND),
+            ("unknown", status.HTTP_422_UNPROCESSABLE_ENTITY),
+            (-8, status.HTTP_422_UNPROCESSABLE_ENTITY),
+        ],
+
+    )
+    async def test_film_get_fail(
+            self, async_test_client: AsyncClient, es_write_data, es_prepared_data, movie_id, expected_status_code
+    ):
+        es_write_data(
+            es_index=self.MOVIE_INDEX,
+            index_mapping=es_movie_index_mapping,
+            data=es_prepared_data(index=self.MOVIE_INDEX, data=movie_data),
+        )
+
+        es_write_data(
+            es_index=self.GENRE_INDEX,
+            index_mapping=es_genres_index_mapping,
+            data=es_prepared_data(index=self.GENRE_INDEX, data=genres_data),
+        )
+
+        response = await async_test_client.get(f"/api/v1/films/{movie_id}")
+
+        assert response.status_code == expected_status_code
+
+    async def test_film_get_success(
+            self, async_test_client: AsyncClient, es_write_data, es_prepared_data, one_movie_data
+    ):
+        es_write_data(
+            es_index=self.MOVIE_INDEX,
+            index_mapping=es_movie_index_mapping,
+            data=es_prepared_data(index=self.MOVIE_INDEX, data=movie_data),
+        )
+
+        es_write_data(
+            es_index=self.GENRE_INDEX,
+            index_mapping=es_genres_index_mapping,
+            data=es_prepared_data(index=self.GENRE_INDEX, data=genres_data),
+        )
+        movie_id = one_movie_data.get('id')
+        response = await async_test_client.get(f"/api/v1/films/{movie_id}")
+
+        assert response.status_code == status.HTTP_200_OK
+        resp_json = response.json()
+        assert resp_json.get('id') == movie_id
+
+    async def test_films_get(
+            self, async_test_client: AsyncClient, es_write_data, es_prepared_data,
+    ):
+        es_write_data(
+            es_index=self.MOVIE_INDEX,
+            index_mapping=es_movie_index_mapping,
+            data=es_prepared_data(index=self.MOVIE_INDEX, data=movie_data),
+        )
+
+        es_write_data(
+            es_index=self.GENRE_INDEX,
+            index_mapping=es_genres_index_mapping,
+            data=es_prepared_data(index=self.GENRE_INDEX, data=genres_data),
+        )
+
+        response = await async_test_client.get("/api/v1/films")
+
+        assert response.status_code == status.HTTP_200_OK
+        resp_json = response.json()
+        assert len(resp_json) == len(movie_data)
+        assert resp_json[0].get('imdb_rating') == 0.0
+
+    async def test_films_get_sort(
+            self, async_test_client: AsyncClient, es_write_data, es_prepared_data,
+    ):
+        es_write_data(
+            es_index=self.MOVIE_INDEX,
+            index_mapping=es_movie_index_mapping,
+            data=es_prepared_data(index=self.MOVIE_INDEX, data=movie_data),
+        )
+
+        es_write_data(
+            es_index=self.GENRE_INDEX,
+            index_mapping=es_genres_index_mapping,
+            data=es_prepared_data(index=self.GENRE_INDEX, data=genres_data),
+        )
+
+        response = await async_test_client.get("/api/v1/films", params={'sort': '-imdb_rating'})
+
+        assert response.status_code == status.HTTP_200_OK
+        resp_json = response.json()
+        assert len(resp_json) == len(movie_data)
+        assert resp_json[0].get('imdb_rating') == 9.0
+        assert resp_json[-1].get('imdb_rating') == 0.0
+
+    async def test_films_get_genre(
+            self, async_test_client: AsyncClient, es_write_data, es_prepared_data, one_genre_data, one_movie_data
+    ):
+        es_write_data(
+            es_index=self.MOVIE_INDEX,
+            index_mapping=es_movie_index_mapping,
+            data=es_prepared_data(index=self.MOVIE_INDEX, data=movie_data),
+        )
+
+        es_write_data(
+            es_index=self.GENRE_INDEX,
+            index_mapping=es_genres_index_mapping,
+            data=es_prepared_data(index=self.GENRE_INDEX, data=genres_data),
+        )
+
+        response = await async_test_client.get("/api/v1/films", params={'genre': one_genre_data.get('id')})
+
+        assert response.status_code == status.HTTP_200_OK
+        resp_json = response.json()
+        assert len(resp_json) == 1
+        assert resp_json[0].get('id') == one_movie_data.get('id')
--- tests/functional/testdata/movie_data.py
+++ tests/functional/testdata/movie_data.py
@@ -3,8 +3,8 @@ import uuid
 movie_data = [
     {
         'id': str(uuid.uuid4()),
-        'imdb_rating': 8.5,
-        'genres': ['Action', 'Sci-Fi'],
+        'imdb_rating': str(i),
+        'genres': [f'Action_{i}', 'Sci-Fi'],
         'title': 'The Star',
         'description': 'New World',
         'directors_names': ['Stan'],
@@ -20,8 +20,30 @@ movie_data = [
         ],
         'directors': [{'id': '5d33e65b-948c-4d73-a037-2779a105dd75', 'name': 'Stan'}],
     }
-    for _ in range(10)
+    for i in range(9)
 ]
+
+cool_movie = {
+        'id': '2f8227c6-444a-4326-b5d6-aa18aca91e55',
+        'imdb_rating': '9.0',
+        'genres': ['Action', 'Sci-Fi'],
+        'title': 'The Star COOL',
+        'description': 'New World',
+        'directors_names': ['Stan'],
+        'actors_names': ['Ann', 'Bob'],
+        'writers_names': ['Ben', 'Howard'],
+        'actors': [
+            {'id': 'ef86b8ff-3c82-4d31-ad8e-72b69f4e3f95', 'name': 'Ann'},
+            {'id': 'fb111f22-121e-44a7-b78f-b19191810fbf', 'name': 'Bob'},
+        ],
+        'writers': [
+            {'id': 'caf76c67-c0fe-477e-8766-3ab3ff2574b5', 'name': 'Ben'},
+            {'id': 'b45bd7bc-2e16-46d5-b125-983d356768c6', 'name': 'Howard'},
+        ],
+        'directors': [{'id': '5d33e65b-948c-4d73-a037-2779a105dd75', 'name': 'Stan'}],
+    }
+
+movie_data.append(cool_movie)
 
 es_movie_index_mapping: dict = {
     "settings": {
